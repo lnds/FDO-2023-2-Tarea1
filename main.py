bdebe0d7236ee32364f9c041682f9da732cd6635
Replit user <>
2023-10-31 16:12:13 +0000

    feat: agrega el endpoint create

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import os
 import psycopg2
-from flask import Flask, render_template
+from flask import Flask, render_template, request, url_for, redirect
 
 VERSION = "v0.1.0"
 
@@ -14,16 +14,9 @@ def get_db_connection():
     return conn
 
 
-@app.route('/')
-def index():
-    conn = get_db_connection()
-    cur = conn.cursor()
-    cur.execute('SELECT * FROM books;')
-    books = cur.fetchall()
-    cur.close()
-    conn.close()
-    return render_template('index.html', books=books, version=VERSION)
-
+@app.route('/create/', methods=('GET', 'POST'))
+def create():
+    return render_template('create.html')
 
 # run the server 
 port = int(os.environ['PORT'])
