f389173b9a09f22ca4ccce9070e6c2a45730bf04
Replit user <>
2023-11-02 21:10:04 +0000

    feat: cambios en main y folders replit

--- main.py
+++ main.py
@@ -13,7 +13,6 @@ def get_db_connection():
                          password=os.environ['DB_PASSWORD'])
     return conn
 
-
 @app.route('/')
 def index():
     conn = get_db_connection()
@@ -24,9 +23,24 @@ def index():
     conn.close()
     return render_template('index.html', books=books, version=VERSION)
 
-
 @app.route('/create/', methods=('GET', 'POST'))
 def create():
+    if request.method == 'POST':
+        title = request.form['title']
+        author = request.form['author']
+        pages_num = int(request.form['pages_num'])
+        review = request.form['review']
+
+        conn = get_db_connection()
+        cur = conn.cursor()
+        cur.execute('INSERT INTO books (title, author, pages_num, review)'
+                    'VALUES (%s, %s, %s, %s)',
+                    (title, author, pages_num, review))
+        conn.commit()
+        cur.close()
+        conn.close()
+        return redirect(url_for('index'))
+      
     return render_template('create.html')
 
 # run the server 
