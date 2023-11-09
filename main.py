15afd118d299534ba67438c2efae1e8df8a86ccc
Replit user <>
2023-11-09 15:26:19 +0000

    feat: implementa inserción de libros en la base de datos

--- main.py
+++ main.py
@@ -24,10 +24,28 @@ def index():
     cur.close()
     conn.close()
     return render_template('index.html', books=books, version=VERSION)
+#@app.route('/create/', methods=('GET', 'POST'))
+#def create():
+#    return render_template('create.html')
 @app.route('/create/', methods=('GET', 'POST'))
 def create():
-    return render_template('create.html')
+    if request.method == 'POST':
+        title = request.form['title']
+        author = request.form['author']
+        pages_num = int(request.form['pages_num'])
+        review = request.form['review']
 
+        conn = get_db_connection()
+        cur = conn.cursor()
+        cur.execute('INSERT INTO books (title, author, pages_num, review)'
+                    'VALUES (%s, %s, %s, %s)',
+                    (title, author, pages_num, review))
+        conn.commit()
+        cur.close()
+        conn.close()
+        return redirect(url_for('index'))
+
+    return render_template('create.html')
 # run the server 
 port = int(os.environ['PORT'])
 app.run(host="0.0.0.0", port=port)
